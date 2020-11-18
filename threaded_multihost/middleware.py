04c83b0ba7e126ac68b9701299de7ed5c7d1b202
Shaun <seyyes2000@hotmail.com>
2020-11-18 16:12:22 -0500

    update middleware for Django 1.11

--- threaded_multihost/middleware.py
+++ threaded_multihost/middleware.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 """ threadlocals Middleware, provides a better, faster way to get at request and user.
-
 :Authors:
    - Herbert Poul http://sct.sphene.net
    - Bruce Kroeze
-
 Branched from [http://code.djangoproject.com/wiki/CookBookthreadlocalsAndUser CookBookThreadLocalsAndUser]
 as modified by [http://sct.sphene.net Sphene Community tools].
 """
 
 from threaded_multihost.threadlocals import set_thread_variable, set_current_user
 
+
 class ThreadLocalMiddleware(object):
     """Middleware that gets various objects from the
     request object and saves them in thread local storage."""
 
-    def process_request(self, request):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
         set_thread_variable('request', request)
         set_current_user(request.user)
-        
+        return self.get_response(request)
